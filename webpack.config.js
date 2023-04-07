1963903a7d8054c3bf9fbae7ed789f206fb7c199
Yuri <player2905@gmail.com>
2023-04-07 16:01:15 +0300

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,22 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 const isProd = process.env.NODE_ENV === 'production' // создали переменные дял определения режима сборки dev/prod, для них доустановили пакет cross-env
 const isDev = !isProd
 
+// const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
+console.log(isDev)
 module.exports = {
+  context: path.resolve(__dirname, 'src'),
   mode: 'development',
   entry: {
-    main: './src/js/main.js',
-    daily: './src/js/daily.js'
+    main: './js/main.js',
+    daily: './js/daily.js'
   },
   output: {
-    filename: '[name].[contenthash].js', // автоматически подставляется имя и хеш при создании нового бандла
+    filename: '[name].bundle.[hash].js', // автоматически подставляется имя и хеш при создании нового бандла
     path: path.resolve(__dirname, 'dist'),
     clean: true // очищает от старых бандлов
   },
@@ -20,32 +25,53 @@ module.exports = {
       chunks: 'all'
     }
   },
+  devtool: isDev ? 'source-map' : false, // добавляем sourcemap в режиме разработки
+    devServer: {
+        port: 3000,
+        hot: isDev
+    },
   plugins: [
     new HtmlWebpackPlugin({
-    template: './src/index.html',
+    template: './index.html',
     chunks: ['main'],
     minify: { // минифицируем сборку html
       removeComments: isProd,
       collapseWhitespace: isProd
   }}),
     new HtmlWebpackPlugin({
-    template: './src/daily.html',
+    template: './daily.html',
     filename: 'daily.html',
     chunks: ['daily'],
     minify: { // минифицируем сборку html
       removeComments: isProd,
       collapseWhitespace: isProd
   }}),
+  new CopyPlugin({ // плагин позволяющий копировать определенные файлы или папки в папку со сброкой
+    patterns: [
+      { from: path.resolve(__dirname, 'src/images'), // в нашем случае копируем favicon
+        to: path.resolve(__dirname, 'dist')
+      },
+    ],
+  }),
+  new MiniCssExtractPlugin({
+    filename: `bundle.[hash].css`
+  })
 ],
-  watch: true,
-
-  devtool: "source-map",
-
+  // watch: true,
   module: {
     rules: [
       {
+        // test: /\.s[ac]ss$/i,
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [
+          // Creates `style` nodes from JS strings
+          // MiniCssExtractPlugin.loader,
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          // "sass-loader",
+        ],
       },
     ],
   },
